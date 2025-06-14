5f253f7326c85476ebf4d63b4fabcf6b2178aca3
arusnmi <122535265+arusnmi@users.noreply.github.com>
2025-06-14 14:58:46 +0000

    ""

---
+++ CSV to SQL converter.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sqlite3
+
+csv_file1 = 'recpies (1).csv'
+db_file1 = 'recpies.db'
+table_name = 'my_table'
+
+# Read CSV into DataFrame
+df = pd.read_csv(csv_file1)
+
+# Connect to SQLite database (it will be created if it doesn't exist)
+conn = sqlite3.connect(db_file1)
+
+# Write the DataFrame to the SQLite table
+df.to_sql(table_name, conn, if_exists='replace', index=False)
+
+conn.close()
